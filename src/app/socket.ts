99cf49995fdca1462b4149cc8c90a890f5d1c6ec
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-09-28 21:44:11 +0530

    init

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -5,6 +5,6 @@ const userId = Cookies.get("SSID");
 const search = new URLSearchParams(
   typeof window !== "undefined" ? window.location.search : ""
 );
-export const socket = io("http://localhost:4000", {
+export const socket = io("https://vibe-node-js.onrender.com", {
   query: { userId, roomId: search.get("room") },
 });
